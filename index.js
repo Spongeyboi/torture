081fe17d74f1e7e20b5e7a5ece889d26ff7edfad
Spongey <71215126+Spongeyboi@users.noreply.github.com>
2022-02-14 19:44:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = require('express')();
-app.get('/',(r,s)=>s.render('./index.html'))
+app.get('/',(r,s)=>s.sendFile('./index.html'))
 app.listen(process.env.PORT);
